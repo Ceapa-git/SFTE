abfb84e892ff41662c72018455772f1897f01dfb
Bugnariu Dan <dan.bugnariu0902@gmail.com>
2023-05-04 15:42:24 +0300

    init

---
+++ headers/pch.h
@@ -0,0 +1,32 @@
+#ifndef __PCH__H__
+#define __PCH__H__
+
+#include <chrono>
+
+// Headers for I/O operations
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+
+// Headers for memory management
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
+
+// Headers for multithreading
+#include <thread>
+#include <mutex>
+#include <future>
+#include <condition_variable>
+
+#endif //__PCH__H__
