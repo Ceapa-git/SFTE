f37773fa2525a3a70225a3040c1a4fa562c9b738
Bugnariu Dan <dan.bugnariu0902@gmail.com>
2023-05-04 17:50:11 +0300

    sfml precompiled header + begining of main window

---
+++ headers/mainwindow.h
@@ -0,0 +1,13 @@
+#ifndef __MAINWINDOW__H__
+#define __MAINWINDOW__H__
+
+#include "pch.h"
+#include "pchSFML.h"
+#include <datafile.h>
+
+namespace sfte
+{
+
+}
+
+#endif //__MAINWINDOW__H__
---
+++ headers/pchSFML.h
@@ -0,0 +1,14 @@
+#ifndef __PCH__SFML__H__
+#define __PCH__SFML__H__
+
+#include <SFML/Audio.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/GpuPreference.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#endif //__PCH__SFML__H__
